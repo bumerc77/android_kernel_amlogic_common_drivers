417920369f77dcafcf8a59149642599c50eaf42d
Cosmin Tanislav <demonsingur@gmail.com>
2026-07-16 03:00:36 +0300

    thermal: meson_tsensor: add meson_get_temperature() export for khadas-mcu
    Change-Id: I5faca9c8ec51cd950b450b5eba03b29beca779ef

--- drivers/thermal/meson_tsensor.c
+++ drivers/thermal/meson_tsensor.c
@@ -154,6 +154,8 @@ struct meson_tsensor_data {
 	void (*tsensor_update_irqs)(struct meson_tsensor_data *data);
 };
 
+static struct meson_tsensor_data *g_tsensor_data_ptr;
+
 static void meson_report_trigger(struct meson_tsensor_data *p)
 {
 	char data[10], *envp[] = { data, NULL };
@@ -554,6 +556,20 @@ static int meson_get_temp(void *p, int *temp)
 		pr_info("[%s %d]-temp[id:%d]:%d\n", __func__, __LINE__, data->id, *temp);
 	return 0;
 }
+
+int meson_get_temperature(void)
+{
+	int temp;
+	int ret;
+
+	ret = meson_get_temp(g_tsensor_data_ptr, &temp);
+	if (ret) {
+		pr_debug("meson_get_temp failed!\n");
+		return ret;
+	}
+	return temp / 1000;
+}
+EXPORT_SYMBOL(meson_get_temperature);
 
 static void meson_tsensor_work(struct work_struct *work)
 {
@@ -1065,6 +1081,9 @@ static int meson_tsensor_probe(struct platform_device *pdev)
 		debugfs_dir, tz, &temp_write_fops);
 	if (!of_property_read_string(pdev->dev.of_node, "cust_governor", &governor_name))
 		aml_verify_governor(tz, governor_name);
+
+	if (data->id == 0)
+		g_tsensor_data_ptr = data;
 out:
 	return 0;
 
